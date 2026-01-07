0ab38feadee38eda7326477464c1bb7ee0bc82da
lliam4343-droid <lliam4343@hotmail.com>
2026-01-07 11:47:33 +0000

    Delete config.v20.js

--- config.v20.js
+++
@@ -1,5 +0,0 @@
-// Google Sheets CSV link (published)
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQnMwNTh_7khrIfKEGWWWV8MSfLDxUDPBBPGOBgGoex6-kLDNXqKYpCJ1cPJEXHr0RbLlJDRNwO_1ie/pub?gid=1134376192&single=true&output=csv";
-
-// Compatibility: app also accepts DATA_CSV_URL
-const DATA_CSV_URL = SHEET_CSV_URL;
